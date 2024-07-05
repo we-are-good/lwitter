fff97f79bc4dcf2996b0314a4486993ecbb2eb4e
we-are-good <140328307+we-are-good@users.noreply.github.com>
2024-07-05 18:25:18 +0900

    [feat] 레이아웃 기능

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,71 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { auth } from "../routes/firebase";
+
+const Wrapper = styled.div`
+  display: grid;
+  gap: 20px;
+  grid-template-columns: 1fr 4fr;
+  padding: 50px 0px;
+  width: 100%;
+  height: 100%;
+  max-width: 860px;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+`;
+const MenuItem = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  cursor: pointer;
+  border: 2px solid white;
+  background-color: white;
+  img {
+    width: 30px;
+    fill: white;
+  }
+  &.log-out {
+    border-color: tomato;
+    background-color: tomato;
+  }
+`;
 
 const Layout = () => {
+  const navigate = useNavigate();
+  const onLogOut = async () => {
+    const logOutConfirm = confirm("정말 로그아웃 하시겠습니까?");
+    if (logOutConfirm) {
+      await auth.signOut();
+      navigate("/login");
+    }
+  };
   return (
-    <>
-      Layout
+    <Wrapper>
+      <Menu>
+        <Link to="/">
+          <MenuItem>
+            <img src="/home.svg" />
+          </MenuItem>
+        </Link>
+        <Link to="/profile">
+          <MenuItem>
+            <img src="/user.svg" />
+          </MenuItem>
+        </Link>
+        <MenuItem className="log-out" onClick={onLogOut}>
+          <img src="/logout.svg" />
+        </MenuItem>
+      </Menu>
       <Outlet />
-    </>
+    </Wrapper>
   );
 };
 
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,13 +1,7 @@
-import { auth } from "./firebase";
-
 const Home = () => {
-  const logOut = () => {
-    auth.signOut();
-  };
   return (
     <>
       <div>home</div>
-      <button onClick={logOut}>Log out</button>
     </>
   );
 };
